ef1c7d95b686ef4e20e881c96fdd26c379ac914b
Gagan Daroach <daroachgb@msoe.edu>
2020-02-02 18:55:30 -0600

    enter running container convenience script

---
+++ enter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# TO BE RUN ON MSOE dh-dgx node
+#
+# Author: Gagan Daroach <gagandaroach@gmail.com>
+user=$USER
+container_name="${user}_researcher"
+
+docker exec -it $container_name /bin/bash
