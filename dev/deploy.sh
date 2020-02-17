0fd087c4f67b7acbed66f539da15a7f6a2a3adbf
Gagan Daroach <daroachgb@msoe.edu>
2020-02-17 16:52:39 -0600

    update container name with latest tag

--- dev/deploy.sh
+++ dev/deploy.sh
@@ -9,6 +9,6 @@ container_name="${user}_researcher_dev"
 docker stop $container_name
 docker rm $container_name
 # build docker container
-docker build . -t $container_name
+docker build . -t $container_name:latest
 
 nvidia-docker run --name $container_name --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -it --rm -v /data:/srv/data -v ~:/srv/hosthome -it -v /raid:/srv/raid $container_name
